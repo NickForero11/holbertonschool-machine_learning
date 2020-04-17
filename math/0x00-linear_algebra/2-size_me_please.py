5dc89679c406f2cdfd45932f5415d7c8ff9eb067
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-04-16 23:59:28 -0500

    Added a function to compute the shape of a matrix

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,24 @@
+"""Module to compute the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix.
+
+    Arguments:
+        matrix (list): the matrix that will be processed
+
+    Returns:
+        tuple:  a tuple that contains the shape of every dimmension
+                in the matrix.
+    """
+    shape = []
+    dimension = matrix[:]
+    while isinstance(dimension, list):
+        size = len(dimension)
+        shape.append(size)
+        if size > 0:
+            dimension = dimension[0]
+        else:
+            break
+
+    return shape
