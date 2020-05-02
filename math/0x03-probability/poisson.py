4608f54023158a7fe4340c12b9729806c5cf8659
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-05-01 19:55:40 -0500

    Updated the Poison Class:
    - Added a function to calculate the value of the PMF for a given number of “successes”.

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -3,9 +3,26 @@
 """
 
 
+def factorial(number):
+    """Compute the factorial of a number.
+
+    Arguments:
+        number (int): an integer number greater than 0
+
+    Returns:
+        int: the factorial of @number.
+    """
+    result = 1
+    while number:
+        result *= number
+        number -= 1
+    return result
+
+
 class Poisson():
     """Class Poisson that represents a Poisson Distribution.
     """
+
     def __init__(self, data=None, lambtha=1.):
         """Initializes a Poisson Distribution.
 
@@ -34,3 +51,19 @@ class Poisson():
                     raise ValueError('data must contain multiple values')
                 else:
                     self.lambtha = sum(data) / num_of_elements
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of “successes”.
+
+        Arguments:
+            k (integer): is the number of “successes”.
+
+        Returns:
+            float: the PMF value for k, 0 otherwise
+        """
+        if k < 0:
+            return 0
+        if not isinstance(k, int):
+            k = int(k)
+        e = 2.7182818285
+        return ((e ** (-self.lambtha) * (self.lambtha ** k)) / factorial(k))
