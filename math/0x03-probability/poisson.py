c6cafe37b680c776740b30c6d41df106b5418392
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-05-01 19:27:20 -0500

    Updated documentation of the Poisson class

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python3
 """Module to represent the Poisson Distribution
 """
 
 
 class Poisson():
+    """Class Poisson that represents a Poisson Distribution.
+    """
     def __init__(self, data=None, lambtha=1.):
+        """Initializes a Poisson Distribution.
+
+        Keyword Arguments:
+            data (List):        a list of the data to be used to estimate
+                                the distribution (default: {None})
+            lambtha (Float):    the expected number of occurrences
+                                in a given time frame (default: {1.})
+
+        Raises:
+            ValueError: If data isn't given and lambtha isn't a positive value.
+            TypeError:  If data isn't a list.
+            ValueError: If data doesn't contain at least two data points.
+        """
         if not data:
             if lambtha < 0:
                 raise ValueError('lambtha must be a positive value')
@@ -18,4 +34,3 @@ class Poisson():
                     raise ValueError('data must contain multiple values')
                 else:
                     self.lambtha = sum(data) / num_of_elements
-
