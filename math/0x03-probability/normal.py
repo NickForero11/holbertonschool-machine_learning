2d43d5d7672f1a45b2899e1893cf046f0e0146c5
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-05-01 23:45:10 -0500

    Added a Class with a minimal representation of a Normal Distribution

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Module to represent the Normal Distribution
+"""
+
+
+class Normal():
+    """Class that represents a Normal Distribution.
+    """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initializes a Normal Distribution.
+
+        Keyword Arguments:
+            data (List):    a list of the data to be used
+                            to estimate the distribution (default: {None})
+            mean (float):   the mean of the distribution (default: {0.})
+            stddev (float): the standard deviation of the distribution
+                            (default: {1.})
+
+        Raises:
+            ValueError: If data isn't given and stddev is not a positive value.
+            TypeError:  If data isn't a list.
+            ValueError: If data doesn't contain at least two data points.
+        """
+        if data is None:
+            self.mean = float(mean)
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            else:
+                self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            else:
+                num_of_elements = len(data)
+                if num_of_elements < 2:
+                    raise ValueError('data must contain multiple values')
+                else:
+                    self.mean = sum(data) / num_of_elements
+                    differences_squared = map(
+                        lambda num: ((num - self.mean) ** 2),
+                        data
+                    )
+                    variance = sum(differences_squared) / num_of_elements
+                    self.stddev = variance ** 0.5  # it means sqrt(variance)
